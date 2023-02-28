72c42b142d8eee6fcec954e880f37336b72783a1
David Garcia Gonzalez <dgarci23@nd.edu>
2023-02-28 13:30:11 -0500

    Add email alerts

--- amplify/backend/function/hslambda/src/app.js
+++ amplify/backend/function/hslambda/src/app.js
@@ -5,15 +5,13 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
-
-
-
 const AWS = require('aws-sdk')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const { json } = require('body-parser')
 const bodyParser = require('body-parser')
 const express = require('express')
 const {v4: uuidv4} = require('uuid')
+
 
 AWS.config.update({ region: process.env.TABLE_REGION });
 
@@ -261,15 +259,6 @@ app.put(userPath+hashKeyPath, function(req, res) {
     req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
   }
 
-  /*let postItemParams = {
-    TableName: tableName,
-    Item: {
-      userId: req.params.userId,
-      name: req.query.name,
-      phone: req.query.phone,
-      email: req.query.email
-    }
-  }*/
   let postItemParams = {
     TableName: tableName,
     Key: {
@@ -299,11 +288,30 @@ app.put(userPath+hashKeyPath, function(req, res) {
 
 // PUT /sensor/:userID, update sensor
 app.put(sensorPath+hashKeyPath, function(req, res) {
+  
+  if (req.query.sensorStatus === "triggered") {
+    let searchParams = {
+      TableName: tableName,
+      Key: {
+        userId: req.params.userId
+      }
+    }
+  
+    dynamodb.get(searchParams, (err, data) => {
+      if (err) {
+        res.status(500);
+        res.json({error: 'Could not load items: ' + err});
+      } else {
+        sendEmail(data.Item.email, data.Item.name, data.Item.sensors[req.query.sensorId])
+      }
+    });
+
+    
+  }
 
   if (userIdPresent) {
     req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
   }
-
   let postItemParams = {
     TableName: tableName,
     Key: {
@@ -316,6 +324,10 @@ app.put(sensorPath+hashKeyPath, function(req, res) {
     ExpressionAttributeNames: {
       "#m": req.query.sensorId
     }
+  };
+  if (req.query.sensorStatus === "triggered") {
+    postItemParams.ConditionExpression = "sensors.#m.sensorStatus <> :disarmed";
+    postItemParams.ExpressionAttributeValues[":disarmed"] = "disarmed";
   }
   dynamodb.update(postItemParams, (err, data) => {
     if (err) {
@@ -326,6 +338,48 @@ app.put(sensorPath+hashKeyPath, function(req, res) {
     }
   });
 });
+
+const sendEmail = (email, name, sensor) => {
+  var emailParams = {
+    Destination: {
+      ToAddresses: [
+        email
+      ]
+    },
+    Message: {
+      Body: {
+        Html: {
+         Charset: "UTF-8",
+         Data: `Hello ${name}. The ${sensor.sensorType} sensor ${sensor.sensorName} was triggered.`
+        },
+        Text: {
+         Charset: "UTF-8",
+         Data: `<h1>Hello ${name}. </h1>The ${sensor.sensorType} sensor ${sensor.sensorName} was triggered.`
+        }
+       },
+       Subject: {
+        Charset: 'UTF-8',
+        Data: 'ALERT: SENSOR TRIGGERED'
+       }
+      },
+    Source: 'fa23.hs2.alerts@gmail.com',
+    ReplyToAddresses: [
+       'fa23.hs2.alerts@gmail.com'
+    ],
+  };
+
+  // Create the promise and SES service object
+  var sendPromise = new AWS.SES().sendEmail(emailParams).promise();
+  
+  // Handle promise's fulfilled/rejected states
+  sendPromise.then(
+    function(data) {
+      console.log(data.MessageId);
+    }).catch(
+      function(err) {
+        console.error(err, err.stack);
+    });
+}
 
 app.listen(3000, function() {
   console.log("App started")
